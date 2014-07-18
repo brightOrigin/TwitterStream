d7885b80006c5f62bd628657eec8da2bc8fce0d6
brightOrigin <tony@brightorigin.com>
2014-07-18 13:08:10 -0700

    Initial Commit

---
+++ TwitterStream/BOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BOAppDelegate.h
+//  TwitterStream
+//
+//  Created by Tony Papale on 7/18/14.
+//  Copyright (c) 2014 Bright Origin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwitterStream/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterStream
+//
+//  Created by Tony Papale on 7/18/14.
+//  Copyright (c) 2014 Bright Origin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BOAppDelegate class]));
+    }
+}
